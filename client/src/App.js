a4694db39b8549af4223d88711f5bfc6506dc581
sungjinWi <rkdlem94@gmail.com>
2022-05-31 12:17:43 +0900

    eth.blockNumber

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import { useState } from 'react';
 import axios from "axios"
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { useWeb3React } from '@web3-react/core';
+import {ethers} from "ethers"
 
 function App() {
+  const eth = new ethers.providers.JsonRpcProvider("http://localhost:8001")
   const injected = new InjectedConnector();
 
   const {
@@ -33,6 +35,8 @@ function App() {
 
   const submitHandler = async (e) => {
     e.preventDefault();
+  console.log(eth.blockNumber)
+  // console.log( (await eth.getBalance("0x9f1c6b2f78D504107c496aF83E5f2f2140e7b5d3")).toString())
     // const {data} = await axios.post("http://localhost:3500/vote",{vote})
     // console.log(data)
   }
@@ -76,10 +80,10 @@ function App() {
               onChange={changeHandler}
             />
           </div>
-        
+          <div>chainId:{chainId} account : {account}</div>
         <button type='submit'>제출</button>
       </Form>
-      <button onClick={onClick}>메타마스크 연결</button>
+      <button onClick={onClick}>{!active ? "메타마스크 연결" : "연결 끊기"}</button>
     </div>
   );
 }
