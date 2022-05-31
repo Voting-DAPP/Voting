2416e7f0d79f6b7f3fd566704eea065427c5054e
seungjackie <qkrtmdwo28@gmail.com>
2022-05-31 10:39:36 +0900

    asdfasdf

--- client/src/index.js
+++ client/src/index.js
@@ -2,26 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Web3ReactProvider } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
-
-function getLibrary(provider) {
-  const library = new Web3Provider(provider, "any");
-  return library;
-}
+import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Web3ReactProvider getLibrary={getLibrary}>
+    <BrowserRouter>
       <App />
-    </Web3ReactProvider>
-  </React.StrictMode>
+    </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
